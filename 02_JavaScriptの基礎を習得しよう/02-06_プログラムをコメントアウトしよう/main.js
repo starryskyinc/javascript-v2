b261368bd6d4bb042f4d0be93e2a2409a1292175
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-02-16 11:09:04 +0900

    コメントアウト 追加

---
+++ 02_JavaScriptの基礎を習得しよう/02-06_プログラムをコメントアウトしよう/main.js
@@ -0,0 +1,12 @@
+'use strict';
+// console.log('Hello World!');
+
+/*
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+*/
+
+// console.log('Hello World!');
+// console.log('Hello World!');
+// console.log('Hello World!');
