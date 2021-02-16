04891844b0269107b1a008ad886c5c61c1696ab0
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-02-16 22:09:40 +0900

    変数と定数

---
+++ 02_JavaScriptの基礎を習得しよう/02-09_エスケープシーケンス/main.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log('Hello! I\'m Yuta Nakamura.');
+console.log('\¥ \t AAA \n BBB');
+
+// 補足
+console.log("Hello! I'm Yuta Nakamura.");
+console.log('こんにちは！"中村祐太"さん');
+
+console.log('Hello');
+console.log("Hello");
