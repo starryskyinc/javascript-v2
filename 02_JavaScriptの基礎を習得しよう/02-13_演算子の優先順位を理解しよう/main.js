0311f365b5019d37d79a270ce27260edc15ba480
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-02-17 11:25:21 +0900

    演算子の優先順位

---
+++ 02_JavaScriptの基礎を習得しよう/02-13_演算子の優先順位を理解しよう/main.js
@@ -0,0 +1,18 @@
+'use strict';
+let x = 1;
+let y = 2;
+let z = 3;
+
+let ans1 = x + y * z;
+console.log(ans1);
+
+let ans2 = (x + y) * z;
+console.log(ans2);
+
+// () * / + -
+
+let ans3 = y * z + x;
+console.log(ans3);
+
+let ans4 = (y * z) + x;
+console.log(ans4);
