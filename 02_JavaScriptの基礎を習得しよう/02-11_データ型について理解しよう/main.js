b5bcb9dda78b75b1db1bc1ad1256ab3ae2e53626
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-02-17 10:28:59 +0900

    データ型

---
+++ 02_JavaScriptの基礎を習得しよう/02-11_データ型について理解しよう/main.js
@@ -0,0 +1,19 @@
+'use strict';
+let myName = 'Nakamura';
+let num = 123;
+let dec = 123.456;
+
+console.log(myName);
+console.log(num);
+console.log(dec);
+
+console.log(typeof myName);
+console.log(typeof num);
+console.log(typeof dec);
+
+myName = 456;
+console.log(myName);
+console.log(typeof myName);
+
+// 動的型付け言語
+// 静的型付け言語
