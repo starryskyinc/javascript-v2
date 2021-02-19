bb32f61cd73723b5f887fcbacb80f0e36b26fe1d
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-02-19 09:51:58 +0900

    インクリメントとデクリメント

---
+++ 02_JavaScriptの基礎を習得しよう/02-17_インクリメントとデクリメント/main.js
@@ -0,0 +1,23 @@
+'use strict';
+// 後置演算子
+// let num = 10;
+// num++;
+// console.log(num);
+
+// num--;
+// console.log(num);
+
+// 前置演算子
+// ++num;
+// console.log(num);
+
+// --num;
+// console.log(num);
+
+let num1 = 10;
+let num2 = 10;
+let ans1 = ++num1;
+let ans2 = num2++;
+
+console.log(ans1);
+console.log(ans2);
