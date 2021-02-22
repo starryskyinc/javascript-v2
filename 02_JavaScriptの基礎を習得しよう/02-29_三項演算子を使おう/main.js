468458571b559f241d8c71a5f95db55824d01236
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-02-22 11:58:02 +0900

    三項演算子

---
+++ 02_JavaScriptの基礎を習得しよう/02-29_三項演算子を使おう/main.js
@@ -0,0 +1,12 @@
+'use strict';
+// 構文: 三項演算子
+// 条件式 ? trueの場合 : falseの場合
+
+// 例題）
+// 年齢が20歳以上の場合は、ビールと出力.
+// 年齢が20歳未満の場合は、コーラと出力.
+
+let age = 19;
+let beverage = (age >= 20) ? 'ビール' : 'コーラ';
+console.log(beverage);
+
--- 02_JavaScriptの基礎を習得しよう/main.js
+++ 02_JavaScriptの基礎を習得しよう/main.js
@@ -1,22 +1,3 @@
 'use strict';
-// undefined
-// 未定義。変数を宣言したが、値は代入していない状態。
 
-// null
-// 変数は定義されているが、該当する値が無い状態。
 
-// undefined
-let x;
-console.log(x);
-
-// null
-let item = null;
-console.log(item);
-
-if(x === undefined) {
-  console.log('undefinedです');
-}
-
-if(item === null) {
-  console.log('nullです');
-}
