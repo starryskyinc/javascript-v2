6b5be154df7fe04b102c5fb3fc5db321e1d69010
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-03-12 14:16:43 +0900

    アロー関数

---
+++ 03_JavaScriptの基礎を習得しよう_2/03-06_アロー関数を使おう/main.js
@@ -0,0 +1,11 @@
+'use strict';
+// 例題:四角形の面積を求める関数
+// const getRectangle = function(height, width) {
+//   return height * width;
+// }
+// console.log(getRectangle(3, 5));
+
+const getRectangle = (height, width) => {
+  return height * width;
+};
+console.log(getRectangle(3, 5));
