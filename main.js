8649c3ccd1e3ed8c73bb4bd8969c6d4be8acd492
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-03-12 14:04:55 +0900

    アロー関数

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 'use strict';
 // 例題:四角形の面積を求める関数
-const getRectangle 
-  = new Function('height', 'width', 'return height * width');
+// const getRectangle = function(height, width) {
+//   return height * width;
+// }
+// console.log(getRectangle(3, 5));
 
+const getRectangle = (height, width) => {
+  return height * width;
+};
 console.log(getRectangle(3, 5));
