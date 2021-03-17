8f380977ea9093801ba61c2ebe5918f366542e10
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-03-17 13:15:33 +0900

    forEach

---
+++ 03_JavaScriptの基礎を習得しよう_2/03-18_繰り返し処理forEachを使おう/main.js
@@ -0,0 +1,11 @@
+'use strict';
+// 例題) 配列から一つずつ、
+// 要素を取り出して、コンソールに出力
+let colors = ['Red', 'Green', 'Blue'];
+// for(let i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
+// }
+
+colors.forEach(function(color){
+  console.log(color)
+});
