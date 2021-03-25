b52e688b5aaabada044dffdabdbf7f2be75814d6
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-03-25 10:07:40 +0900

    演習：配列、ループ

---
+++ 03_JavaScriptの基礎を習得しよう_2/03-18_繰り返し処理for_ofを使おう/main.js
@@ -0,0 +1,10 @@
+'use strict';
+// const colors = ['Red', 'Green', 'Blue'];
+// for(const color of colors) {
+//   console.log(color);
+// }
+
+const str = 'JavaScript';
+for(const value of str) {
+  console.log(value);
+}
