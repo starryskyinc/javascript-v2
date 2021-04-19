94acb3432e60c45dc7e5cc31af6b8cfd03037689
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-04-19 09:43:20 +0900

    07-03 HTML

---
+++ 07_フォトギャラリー/07-03_HTML側を実装しよう/main.js
@@ -0,0 +1,8 @@
+'use strict';
+const images = [
+  {src: 'https://source.unsplash.com/C0zDWAPFT9A', description: '猫の画像1'},
+  {src: 'https://source.unsplash.com/fEK4jvgnApg', description: '猫の画像2'},
+  {src: 'https://source.unsplash.com/egfS7HzgKcc', description: '猫の画像3'},
+  {src: 'https://source.unsplash.com/e-S23SJzFqs', description: '猫の画像4'},
+  {src: 'https://source.unsplash.com/Hnwm8ktAd6E', description: '猫の画像5'}
+];
