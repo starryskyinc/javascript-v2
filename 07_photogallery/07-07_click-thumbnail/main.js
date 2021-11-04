00006ebf580350525f06a2cd20a4804f7ae4bf65
naka <9111754+ynakayu@users.noreply.github.com>
2021-11-04 17:52:17 +0900

    unsplash to s3

--- 07_photogallery/07-07_click-thumbnail/main.js
+++ 07_photogallery/07-07_click-thumbnail/main.js
@@ -1,10 +1,10 @@
 'use strict';
 const images = [
-  {src: 'https://source.unsplash.com/C0zDWAPFT9A', description: '猫の画像1'},
-  {src: 'https://source.unsplash.com/fEK4jvgnApg', description: '猫の画像2'},
-  {src: 'https://source.unsplash.com/egfS7HzgKcc', description: '猫の画像3'},
-  {src: 'https://source.unsplash.com/e-S23SJzFqs', description: '猫の画像4'},
-  {src: 'https://source.unsplash.com/Hnwm8ktAd6E', description: '猫の画像5'}
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/C0zDWAPFT9A.jpg', description: '猫の画像1'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/fEK4jvgnApg.jpg', description: '猫の画像2'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/egfS7HzgKcc.jpg', description: '猫の画像3'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/e-S23SJzFqs.jpg', description: '猫の画像4'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/Hnwm8ktAd6E.jpg', description: '猫の画像5'}
 ];
 
 // メイン画像の初期値を設定
